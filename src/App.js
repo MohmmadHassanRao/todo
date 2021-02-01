99745fe16a9ce2b52abe4fd6f63b9ec5059ad16c
hassan <hassanribery0335@gmail.com>
2021-02-01 17:38:03 +0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  let [todos,setTodos] = useState([])
+  let todo = useRef()
+
+  const addTodo =()=>{
+    setTodos([...todos,todo.current.value])
+    todo.current.value =''
+  }
+  const delTodo=(i)=>{
+    console.log("index is = > " ,i);
+    var newTodo = [...todos];
+    console.log("new todo is => " ,newTodo);
+    newTodo.splice(i,1);
+    setTodos(newTodo);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>  
+      <input ref={todo}/><button onClick={addTodo}>Add</button>
+      {
+        todos.map((todo,i)=>{
+          return(
+            <div key={i}>
+              {todo}
+            <button onClick={(e)=>{delTodo(i)}}>Delete</button>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
