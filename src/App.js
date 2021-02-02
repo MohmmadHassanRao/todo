8f03153bc355228c0e65fa7c7ff42a5ef53550a2
hassan <hassanribery0335@gmail.com>
2021-02-02 16:50:38 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { useRef, useState } from 'react';
+import { useRef, useState , useEffect} from 'react';
 import './App.css';
+
 
 function App() {
   let [todos,setTodos] = useState([])
   let todo = useRef()
+  
+  
+ 
+  console.log(todo.current);
+
 
   const addTodo =()=>{
     setTodos([...todos,todo.current.value])
@@ -15,18 +21,29 @@ function App() {
     console.log("new todo is => " ,newTodo);
     newTodo.splice(i,1);
     setTodos(newTodo);
+    
+  }
+  const editTodo = (e) =>{
+   var edit = prompt("Edit your todo")
+   var currentTodo = [...todos]
+   currentTodo[e] = edit
+    setTodos(currentTodo)
   }
   return (
     <div className="App">
+     
       <h1>Todo App</h1>  
       <input ref={todo}/><button onClick={addTodo}>Add</button>
       {
         todos.map((todo,i)=>{
           return(
-            <div key={i}>
+         
+              <div key={i} >
               {todo}
             <button onClick={(e)=>{delTodo(i)}}>Delete</button>
+            <button onClick={(e)=>{editTodo(i)}}>Edit</button>
             </div>
+          
           )
         })
       }
